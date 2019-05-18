f661d74227b179c1277ae0beff800f6b37b9821b
devopsstudio99 <50159118+devopsstudio99@users.noreply.github.com>
2019-05-18 15:36:06 +0700

    Update test_server.js

--- test_server.js
+++ test_server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Welcome');
-}).listen(3001, "0.0.0.0");
+}).listen(3001, "serverip");
 console.log('Server running at http://10.0.2.10:3001/');
